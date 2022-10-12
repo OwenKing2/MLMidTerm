072fa705005af36d8f49af2416b54fcef5f6bfd9
Owen King <tuj66158@temple.edu>
2022-10-11 21:50:20 -0400

    Added in a few features to work with.  Currently the bigram and trigrams need work, I think there is some problem with trying to take the length of them before or after iterating through them, it is causing the generator to go blank.

--- ParaphraseIdentification.py
+++ ParaphraseIdentification.py
@@ -4,7 +4,6 @@ from IPython.display import display
 from tabulate import tabulate
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import nltk
 from nltk.corpus import stopwords
@@ -14,6 +13,8 @@ import math
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import defaultdict
 import codecs
+import string
+import itertools
 
 header = ["train_id", "Sentence_1", "Sentence_2", "Output"]
 
@@ -22,6 +23,7 @@ df = pd.read_csv('train_with_label.txt', sep='\t', on_bad_lines='skip', names=he
 # Preprocessing
 # nltk.download('omw-1.4')
 # nltk.download('stopwords')
+# nltk.download('punkt')
 stop_words = set(stopwords.words('english'))
 df['Sentence_1'] = df['Sentence_1'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
 df['Sentence_2'] = df['Sentence_2'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
@@ -36,11 +38,81 @@ def lemmatize_text(text):
     return st
 
 
+# gets roots of words for comparison
 df['Sentence_1'] = df['Sentence_1'].apply(lemmatize_text)
 df['Sentence_2'] = df['Sentence_2'].apply(lemmatize_text)
 
+# Remove punctuation
+df['Sentence_1'] = df['Sentence_1'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+df['Sentence_2'] = df['Sentence_2'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+
+# Make all words lowercase
+df['Sentence_1'] = df['Sentence_1'].str.lower()
+df['Sentence_2'] = df['Sentence_2'].str.lower()
+
 removedNull = df.dropna()
-X = removedNull[["Sentence_1", "Sentence_2"]]
+
+
+# This is where the mistake is, need to make a function to define the features
+# from the input sentences, so that I can run the same function on the DEV and TEST sets
+# Current feature ideas are, ngram matching (thinking 1 and 2 grams)
+# Actually looks like I can use the CountVectorizer, from sklearn
+# Just need to specify the correct features
+
+def feature_extractor(sentence1array, sentence2array):
+    features = pd.DataFrame(columns=['Length Comparison', 'Proportion of Matching Words', "2grams", "3grams"])
+
+    # Length Dissimilarity: If one is much shorter or longer, it will be a higher value
+    length = []
+    for (sentence_1, sentence_2) in itertools.zip_longest(sentence1array, sentence2array):
+        length.append(abs(len(sentence_1) - len(sentence_2)))
+    features['Length Comparison'] = length
+
+    # total number of matching words divided by total number of unique words
+    matching = []
+    for (sentence_1, sentence_2) in itertools.zip_longest(sentence1array, sentence2array):
+        combined_sentence = sentence_1 + sentence_2
+        unique_words = len(set(combined_sentence.split(' ')))
+        sentence_1_words = sentence_1.split(" ")
+        sentence_2_words = sentence_2.split(" ")
+        common_words = len(list(set(sentence_1_words) & set(sentence_2_words)))
+        matching.append(common_words / unique_words)
+    features['Proportion of Matching Words'] = matching
+
+    # ngrams matches (currently checking 2 and 3 grams)
+    twograms = []
+    threegrams = []
+    for (sentence_1, sentence_2) in itertools.zip_longest(sentence1array, sentence2array):
+        sentence_1_words = nltk.word_tokenize(sentence_1)
+        sentence_2_words = nltk.word_tokenize(sentence_2)
+        sentence_1_bigrams = nltk.bigrams(sentence_1_words)
+        sentence_1_trigrams = nltk.trigrams(sentence_1_words)
+        sentence_2_bigrams = nltk.bigrams(sentence_2_words)
+        sentence_2_trigrams = nltk.trigrams(sentence_2_words)
+
+        # Have to make these since generator objects are used up after loop, so to get length we make a list copy
+        count_sentence_2_bigrams = list(sentence_2_bigrams)
+        count_sentence_2_trigrams = list(sentence_2_trigrams)
+        # I will use the total number of bigrams and trigrams in sentence 2 as the bottom of my ratio
+        bigramMatches = 0
+        for phrase in sentence_1_bigrams:
+            if phrase in sentence_2_bigrams:
+                bigramMatches += 1
+        trigramMatches = 0
+        for phrase in sentence_1_trigrams:
+            if phrase in sentence_2_trigrams:
+                trigramMatches += 1
+        twograms.append(bigramMatches / len(count_sentence_2_bigrams))
+        threegrams.append(trigramMatches / len(list(count_sentence_2_trigrams)))
+    features['2grams'] = twograms
+    features['3grams'] = threegrams
+
+    # synonyms and hypernyms to be implemented later, want to focus on getting some level of output first
+
+    return features
+
+
+X = feature_extractor(removedNull["Sentence_1"], removedNull["Sentence_2"])
 y = removedNull["Output"]
 
 dev = pd.read_csv('dev_with_label.txt', sep='\t', on_bad_lines='skip', names=header, engine='python')
@@ -65,60 +137,4 @@ ydev = removedNull["Output"]
 # removedNull = df.dropna()
 # Xtest = removedNull[["Sentence_1", "Sentence_2"]]
 
-# Now, we need to define a corpus.
-# I will use all the sentence pairs that appear in the training set
-
-vec = CountVectorizer()
-X = vec.fit_transform(X)
-vocab = vec.get_feature_names_out()
-X = X.toarray()
-word_counts = {}
-for l in range(2):
-    word_counts[l] = defaultdict(lambda: 0)
-for i in range(X.shape[0]):
-    l = y[i]
-    for j in range(len(vocab)):
-        word_counts[l][vocab[j]] += X[i][j]
-
-
-def laplace_smoothing(n_label_items, vocab, word_counts, word, text_label):
-    a = word_counts[text_label][word] + 1
-    b = n_label_items[text_label] + len(vocab)
-    return math.log(a / b)
-
-def group_by_label(x, y, labels):
-    data = {}
-    for l in labels:
-        data[l] = x[np.where(y == l)]
-    return data
-
-
-def fit(x, y, labels):
-    n_label_items = {}
-    log_label_priors = {}
-    n = len(x)
-    grouped_data = group_by_label(x, y, labels)
-    for l, data in grouped_data.items():
-        n_label_items[l] = len(data)
-        log_label_priors[l] = math.log(n_label_items[l] / n)
-    return n_label_items, log_label_priors
-
-
-def predict(n_label_items, vocab, word_counts, log_label_priors, labels, x):
-    result = []
-    for text in x:
-        label_scores = {l: log_label_priors[l] for l in labels}
-        words = set(w_tokenizer.tokenize(text))
-        for word in words:
-            if word not in vocab: continue
-            for l in labels:
-                log_w_given_l = laplace_smoothing(n_label_items, vocab, word_counts, word, l)
-                label_scores[l] += log_w_given_l
-        result.append(max(label_scores, key=label_scores.get))
-    return result
-
-
-labels = [0,1]
-n_label_items, log_label_priors = fit(X,y,labels)
-pred = predict(n_label_items, vocab, word_counts, log_label_priors, labels, Xdev)
-print("Accuracy of prediction on test set : ", accuracy_score(ydev,pred))
+#
