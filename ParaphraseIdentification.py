b5c69829f26b677914d95e46459893311655b979
Owen King <tuj66158@temple.edu>
2022-10-20 21:25:43 -0400

    Forgot about requirement that we have to use either SVM or logistic regression, so testing other models is not allowed in this assignment.  They have been removed.

--- ParaphraseIdentification.py
+++ ParaphraseIdentification.py
@@ -38,6 +38,7 @@ def datapreprocess(filename):
     header = ["train_id", "Sentence_1", "Sentence_2", "Output"]
     df = pd.read_csv(filename, sep='\t', on_bad_lines='skip', names=header, engine='python')
     stop_words = set(stopwords.words('english'))
+    # Remove stop words like and, a, the
     df['Sentence_1'] = df['Sentence_1'].apply(
         lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
     df['Sentence_2'] = df['Sentence_2'].apply(
@@ -75,17 +76,12 @@ def bleu_score(sentence1array, sentence2array):
         second_sentence = nltk.word_tokenize(sentence_2)
         bleu_scores.append(round(nltk.translate.bleu_score.sentence_bleu([first_sentence], second_sentence,
                                                                          smoothing_function=smooth.method3)))
-    # for sentence in sentence1array:
-    #     first_sentence = nltk.word_tokenize(sentence)
-    #     for second_phrase in sentence2array:
-    #         second_sentence = nltk.word_tokenize(second_phrase)
-    #         bleu_scores.append(nltk.translate.bleu_score.sentence_bleu([first_sentence], second_sentence))
     features["BLEU_SCORES"] = bleu_scores
     return features
 
 
 def feature_extractor(sentence1array, sentence2array):
-    features = pd.DataFrame(columns=["Length Comparison", "Proportion of Matching Words", "bleu_score"])
+    features = pd.DataFrame(columns=["Length Comparison", "Union", "Proportion of Matching Words", "bleu_score"])
 
     # Length Dissimilarity: If one is much shorter or longer, it will be a higher value
     length = []
@@ -93,59 +89,64 @@ def feature_extractor(sentence1array, sentence2array):
         length.append(abs(len(sentence_1) - len(sentence_2)) / ((len(sentence_1) + len(sentence_2)) / 2))
     features['Length Comparison'] = length
 
-    # total number of matching words divided by total number of unique words
+    # Union: a feature for total number of unique tokens, so that matching words aren't favored too highly
+    # for longer sentences, which could naturally have more
+
+    # Matching: total number of matching words, like 1-grams
     matching = []
+    union = []
     for (sentence_1, sentence_2) in itertools.zip_longest(sentence1array, sentence2array):
         combined_sentence = sentence_1 + sentence_2
         unique_words = len(set(combined_sentence.split(' ')))
         sentence_1_words = sentence_1.split(" ")
         sentence_2_words = sentence_2.split(" ")
         common_words = len(list(set(sentence_1_words) & set(sentence_2_words)))
-        matching.append(common_words / unique_words)
+        matching.append(common_words)
+        union.append(unique_words)
     features['Proportion of Matching Words'] = matching
-
+    features['Union'] = union
     # ngrams matches (currently checking 2 and 3 grams)
-    unigrams = []
-    twograms = []
-    threegrams = []
-    fourgrams = []
-    for (sentence_1, sentence_2) in itertools.zip_longest(sentence1array, sentence2array):
-        sentence_1_words = nltk.word_tokenize(sentence_1)
-        sentence_2_words = nltk.word_tokenize(sentence_2)
-        sentence_1_unigrams = list(nltk.ngrams(sentence_1_words, 1))
-        sentence_2_unigrams = list(nltk.ngrams(sentence_1_words, 1))
-        sentence_1_bigrams = list(nltk.bigrams(sentence_1_words))
-        sentence_1_trigrams = list(nltk.trigrams(sentence_1_words))
-        sentence_2_bigrams = list(nltk.bigrams(sentence_2_words))
-        sentence_2_trigrams = list(nltk.trigrams(sentence_2_words))
-        sentence_1_fourgrams = list(nltk.ngrams(sentence_1_words, 4))
-        sentence_2_fourgrams = list(nltk.ngrams(sentence_1_words, 4))
-
-        # I will use the total number of bigrams and trigrams in sentence 2 as the bottom of my ratio
-        unigram_matches = 0
-        for phrase in sentence_1_unigrams:
-            if phrase in sentence_2_unigrams:
-                unigram_matches += 1
-
-        bigram_matches = 0
-        for phrase in sentence_1_bigrams:
-            if phrase in sentence_2_bigrams:
-                bigram_matches += 1
-
-        trigram_matches = 0
-        for phrase in sentence_1_trigrams:
-            if phrase in sentence_2_trigrams:
-                trigram_matches += 1
-
-        fourgram_matches = 0
-        for phrase in sentence_1_fourgrams:
-            if phrase in sentence_2_fourgrams:
-                fourgram_matches += 1
-
-        unigrams.append(unigram_matches)
-        twograms.append(bigram_matches)
-        threegrams.append(trigram_matches)
-        fourgrams.append(fourgram_matches)
+    # unigrams = []
+    # twograms = []
+    # threegrams = []
+    # fourgrams = []
+    # for (sentence_1, sentence_2) in itertools.zip_longest(sentence1array, sentence2array):
+    #     sentence_1_words = nltk.word_tokenize(sentence_1)
+    #     sentence_2_words = nltk.word_tokenize(sentence_2)
+    #     sentence_1_unigrams = list(nltk.ngrams(sentence_1_words, 1))
+    #     sentence_2_unigrams = list(nltk.ngrams(sentence_1_words, 1))
+    #     sentence_1_bigrams = list(nltk.bigrams(sentence_1_words))
+    #     sentence_1_trigrams = list(nltk.trigrams(sentence_1_words))
+    #     sentence_2_bigrams = list(nltk.bigrams(sentence_2_words))
+    #     sentence_2_trigrams = list(nltk.trigrams(sentence_2_words))
+    #     sentence_1_fourgrams = list(nltk.ngrams(sentence_1_words, 4))
+    #     sentence_2_fourgrams = list(nltk.ngrams(sentence_1_words, 4))
+    #
+    #     # I will use the total number of bigrams and trigrams in sentence 2 as the bottom of my ratio
+    #     unigram_matches = 0
+    #     for phrase in sentence_1_unigrams:
+    #         if phrase in sentence_2_unigrams:
+    #             unigram_matches += 1
+    #
+    #     bigram_matches = 0
+    #     for phrase in sentence_1_bigrams:
+    #         if phrase in sentence_2_bigrams:
+    #             bigram_matches += 1
+    #
+    #     trigram_matches = 0
+    #     for phrase in sentence_1_trigrams:
+    #         if phrase in sentence_2_trigrams:
+    #             trigram_matches += 1
+    #
+    #     fourgram_matches = 0
+    #     for phrase in sentence_1_fourgrams:
+    #         if phrase in sentence_2_fourgrams:
+    #             fourgram_matches += 1
+    #
+    #     unigrams.append(unigram_matches)
+    #     twograms.append(bigram_matches)
+    #     threegrams.append(trigram_matches)
+    #     fourgrams.append(fourgram_matches)
 
     # features['1grams'] = unigrams
     # features['2grams'] = twograms
@@ -158,9 +159,17 @@ def feature_extractor(sentence1array, sentence2array):
     return features
 
 
+def vectorize_features(sentence1array, sentence2array):
+    return []
+
+
 training_data = datapreprocess("train_with_label.txt")
 X = feature_extractor(training_data["Sentence_1"], training_data["Sentence_2"])
 y = training_data["Output"]
+
+dev_data = datapreprocess("dev_with_label.txt")
+Xdev = feature_extractor(dev_data["Sentence_1"], dev_data["Sentence_2"])
+ydev = dev_data["Output"]
 
 SVC = make_pipeline(StandardScaler(), SVC(kernel="sigmoid"))
 SVC.fit(X, y)
@@ -168,28 +177,9 @@ SVC.fit(X, y)
 linearSVC = make_pipeline(StandardScaler(), LinearSVC(dual=False, tol=1e-5))
 linearSVC.fit(X, y)
 
-sgdClassifier = make_pipeline(StandardScaler(), SGDClassifier(loss="perceptron", learning_rate="optimal", eta0=0.01, alpha=0.00001))
-sgdClassifier.fit(X, y)
-
 logisticRegression = make_pipeline(StandardScaler(), LogisticRegression())
 logisticRegression.fit(X, y)
 
-multiNomialNB = make_pipeline(MinMaxScaler(), MultinomialNB())
-multiNomialNB.fit(X, y)
-
-randomForest = make_pipeline(MinMaxScaler(), RandomForestClassifier(bootstrap=True, class_weight="balanced"))
-randomForest.fit(X, y)
-
-dev_data = datapreprocess("dev_with_label.txt")
-Xdev = feature_extractor(dev_data["Sentence_1"], dev_data["Sentence_2"])
-ydev = dev_data["Output"]
-
 print("SVC model accuracy: " + str(SVC.score(Xdev, ydev)))
 print("linearSVC model accuracy: " + str(linearSVC.score(Xdev, ydev)))
-# Stochastic gradient descent has given me the highest accuracy scores, but is wildly inconsistent
-# It has given me scores ranging from ~ 38-70% accuracy.  Perceptron loss model seems like it gives the highest
-# Accuracy, now just need to work on making it more consistent
-print("Stochastic Gradient Descent model accuracy: " + str(sgdClassifier.score(Xdev, ydev)))
 print("Logistic Regression model accuracy: " + str(logisticRegression.score(Xdev, ydev)))
-print("Multinomial Naive Bayes model accuracy: " + str(multiNomialNB.score(Xdev, ydev)))
-print("Random Forest Ensemble model accuracy: " + str(randomForest.score(Xdev, ydev)))
